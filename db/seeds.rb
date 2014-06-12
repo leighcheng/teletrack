089f1aadcb0e9f9248613cc5e4f64582437243ab
Lei Chen <leighcheng@gmail.com>
2014-06-11 17:05:23 -0700

    seeded more initial records

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 staff=Staff.create!(:email=>'admin@yahoo.com',:name=>'admin',:password=>'admin')
+
+Priority.create(priority_name: 'High')
+Priority.create(priority_name: 'Medium')
+Priority.create(priority_name: 'Low')
+
+Type.create(type_name: 'Project')
+Type.create(type_name: 'Knowledge')
+
+
+Status.create(status_name: 'Assigned')
+Status.create(status_name: 'Completed')
+
+Component.create(component_name: 'CCMA')
+Component.create(component_name: 'PBX')
+Component.create(component_name: 'RUBY')
+Component.create(component_name: 'REPORTS')
